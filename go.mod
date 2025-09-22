9698349780f9b93b5d95b013db69dd2ae0fbdeba
Kelvin <denlinato@gmail.com>
2025-09-22 18:13:54 +0000

    first commit

---
+++ go.mod
@@ -0,0 +1,20 @@
+module github.com/kelvin950/sqspubsub
+
+go 1.24.4
+
+require (
+	github.com/ThreeDotsLabs/watermill v1.5.1
+	github.com/ThreeDotsLabs/watermill-aws v1.0.1
+	github.com/aws/aws-sdk-go-v2 v1.39.0
+	github.com/aws/aws-sdk-go-v2/service/sqs v1.42.6
+	github.com/aws/smithy-go v1.23.0
+)
+
+require (
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.4.7 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.7.7 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/lithammer/shortuuid/v3 v3.0.7 // indirect
+	github.com/oklog/ulid v1.3.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,90 @@
+package main
+
+
+import (
+	"context"
+	"net/url"
+
+	"github.com/ThreeDotsLabs/watermill"
+	"github.com/ThreeDotsLabs/watermill-aws/sqs"
+	"github.com/ThreeDotsLabs/watermill/message"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	amazonsqs "github.com/aws/aws-sdk-go-v2/service/sqs"
+	transport "github.com/aws/smithy-go/endpoints"
+)
+
+type SqsSub struct {
+	Subscriber *sqs.Subscriber
+	AwsConfig  *aws.Config
+	Logger     watermill.LoggerAdapter
+	Publisher *sqs.Publisher
+	SqsOpt  []func(*amazonsqs.Options)
+}
+
+func NewSqsSub(awsconfig *aws.Config ,logger watermill.LoggerAdapter) (*SqsSub, error) {
+
+	uri, err := url.Parse("http://localstack:4566")
+	if err != nil {
+		return nil, err
+	}
+	 SqsSub := &SqsSub{
+		AwsConfig: awsconfig, 
+		Logger: logger,
+		SqsOpt: []func(*amazonsqs.Options){
+		amazonsqs.WithEndpointResolverV2(sqs.OverrideEndpointResolver{
+			Endpoint: transport.Endpoint{
+				URI: *uri,
+			},
+		}),
+	},
+	 }
+
+	err = SqsSub.CreateSqsSub() 
+
+	 return SqsSub ,err
+}
+
+func (s *SqsSub) CreateSqsSub() error {
+
+	subscriberConfig := sqs.SubscriberConfig{
+		AWSConfig: *s.AwsConfig,
+		OptFns:    s.SqsOpt,
+	}
+
+	subscriber, err := sqs.NewSubscriber(subscriberConfig, s.Logger)
+	if err != nil {
+		return err
+	}
+
+	s.Subscriber = subscriber
+	return nil
+}
+
+func (s *SqsSub) SetSubToTopic(topic string) (<-chan *message.Message, error) {
+
+	messages, err := s.Subscriber.Subscribe(context.Background(), topic)
+	if err != nil {
+		return nil, err
+	}
+
+	return messages, nil
+
+}
+
+func(s *SqsSub)CreatePub()error{
+		publisherConfig := sqs.PublisherConfig{
+		AWSConfig: aws.Config{
+			Credentials: aws.AnonymousCredentials{},
+		},
+		OptFns: s.SqsOpt,
+	}
+
+	publisher, err := sqs.NewPublisher(publisherConfig, s.Logger)
+	if err != nil {
+		return err 
+	}
+
+	s.Publisher = publisher 
+
+	return nil
+}
